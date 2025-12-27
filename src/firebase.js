04678ce35aa2a94908635d69002f19212e953ed7
DevilTanuki99 <wibuakun2596@gmail.com>
2025-12-28 04:04:16 +0700

    hide api

--- src/firebase.js
+++ src/firebase.js
@@ -4,14 +4,15 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCCGknSQhKlgn9O3MkLIljB9VE7VJ9qIpY",
-  authDomain: "kost-life.firebaseapp.com",
-  projectId: "kost-life",
-  storageBucket: "kost-life.firebasestorage.app",
-  messagingSenderId: "413875678174",
-  appId: "1:413875678174:web:16ed13e8b192177cae4b4f",
-  measurementId: "G-PRV25CV2RS"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
+
 
 
 const app = initializeApp(firebaseConfig);
